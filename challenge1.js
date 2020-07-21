4206fec2eb4c5bbab45503a567add1b24c4c58c8
Meera Sundar <meerasndr@users.noreply.github.com>
2020-07-21 09:38:47 +0530

    refactor challenge 1 make generic function

--- challenge1.js
+++ challenge1.js
@@ -1,10 +1,14 @@
-let limit = 1000
-let checkNum
+//If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+//The sum of these multiples is 23.
+//Find the sum of all the multiples of 3 or 5 below 1000.
 let sumOfNums = 0
-for(checkNum = 1; checkNum < 1000; checkNum++)
-{
-  if((checkNum % 3 == 0) || (checkNum % 5 == 0)){
-     sumOfNums += checkNum
-     }
+function sumOfMultiples(limit){
+  for(let checkNum = 1; checkNum < limit; checkNum++)
+  {
+    if((checkNum % 3 == 0) || (checkNum % 5 == 0)){
+       sumOfNums += checkNum
+       }
+  }
 }
+sumOfMultiples(1000)
 console.log(sumOfNums)
