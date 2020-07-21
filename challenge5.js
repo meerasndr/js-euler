80cb433e9019ff67861f7f37033372d80d461187
Meera Sundar <meerasndr@users.noreply.github.com>
2020-07-21 07:42:03 +0530

    add challenge 5

---
+++ challenge5.js
@@ -0,0 +1,11 @@
+let number = 2520
+while(true){
+  if(number%11 === 0 && number%13 === 0 && number%14 === 0 && number%16 === 0 && number%17 === 0 && number%18 === 0 && number%19 === 0 && number%20 === 0){
+      console.log(number)
+      break;
+  }
+  else{
+    number++;
+  }
+  
+}
